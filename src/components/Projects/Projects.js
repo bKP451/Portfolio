08224a4cbfdf52d1ebd259ea2e55ce6c87f65f7a
Bikash Pandey <bkppandey87@gmail.com>
2023-06-02 11:42:37 +0545

    added gym project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import notesTakingApp from "../../Assets/Projects/notes_taking_app.png"
 import bnsWebDemoPic from "../../Assets/Projects/demo_bns_collections.png"
+import yoalGymKathmandu from "../../Assets/Projects/yoal_gym_kathmandu.png"
 
 function Projects() {
   return (
@@ -35,6 +36,17 @@ function Projects() {
               description="Web application for bnscollections.com.np. It is a shop selling normal male clothing accessories. The shop is located at Lalitpur, Nepal. I have just forked the github source code and have started working on it. The web application will allow customers to view and order goods and pay via Nepali payment gateways."
               ghLink="https://github.com/bKP451/BNS--collections"
               demoLink="https://expresscart-demo.markmoffat.com/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={yoalGymKathmandu}
+              isBlog={false}
+              status={"IN DEVELOPMENT"}
+              title="GYM HOUSE KATHMANDU"
+              description="Web application for a gym house located in Kathmandu, Nepal"
+              ghLink="https://github.com/bKP451/YOAL_GYM"
+              demoLink="https://yoal-gym.vercel.app/"
             />
           </Col>
         </Row>
